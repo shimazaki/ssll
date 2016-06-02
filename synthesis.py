87fe5b925934221e44c372bf30c28bc9107d777a
christiando <christiandonner48@gmail.com>
2016-06-02 09:33:12 +0200

    Made Gaussian Kernel more explicit

--- synthesis.py
+++ synthesis.py
@@ -28,20 +28,20 @@ from functools import partial
 import transforms
  
  
-def generate_thetas(N, O, T, mu1=-2., sigma1=50, mu2=0., sigma2=50, alpha=3., ratio_modulated=1.):
+def generate_thetas(N, O, T, mu1=-2., sigma1=50, mu2=0., sigma2=50, alpha=12., ratio_modulated=1.):
     D = transforms.compute_D(N, O)
     MU = numpy.tile(mu1, (T, D))
     MU[:,N:] = mu2
     # Create covariance matrix
     X = numpy.tile(numpy.arange(T),(T,1))
-    K1 = numpy.exp(-(X - X.transpose()) ** 2 / (2. * sigma1 ** 2))
-    K2 = numpy.exp(-(X - X.transpose()) ** 2 / (2. * sigma2 ** 2))
+    K1 = 1./alpha*numpy.exp(-(X - X.transpose()) ** 2 / (2. * sigma1 ** 2))
+    K2 = 1./alpha*numpy.exp(-(X - X.transpose()) ** 2 / (2. * sigma2 ** 2))
     # Generate Gaussian processes
     L1 = numpy.linalg.cholesky(K1 + 1e-13 * numpy.eye(T))
     L2 = numpy.linalg.cholesky(K2 + 1e-13 * numpy.eye(T))
     theta = numpy.empty([T, D])
-    theta[:,:N] = mu1 + numpy.dot(L1, 1./alpha*numpy.random.randn(T, N))
-    theta[:,N:] = mu2 + numpy.dot(L2, 1./alpha*numpy.random.randn(T, D - N))
+    theta[:,:N] = mu1 + numpy.dot(L1, numpy.random.randn(T, N))
+    theta[:,N:] = mu2 + numpy.dot(L2, numpy.random.randn(T, D - N))
     num_non_modulated = int(numpy.around((1. - ratio_modulated)*(D - N)))
     non_modulated_idx = random.sample(range(N,D), num_non_modulated)
     theta[:, non_modulated_idx] = 0.
